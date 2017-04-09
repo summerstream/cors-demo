c52b0faf1c9624008b4b0e1c67eee1bf18f9310f
culnoty <culnoty@outlook.com>
2017-04-09 20:08:46 +0800

    done

--- server.js
+++ server.js
@@ -5,4 +5,28 @@ app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
+app.post('/api1',function(req,res){
+    res.send('Hello');
+});
+
+app.post('/api2',function(req,res){
+    res.send('Hello');
+});
+
+app.post('/api3',function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Content-Type','application/json');
+    res.send('Hello');
+});
+
+app.options('/api3',function(req,res){
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','POST,GET');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type');
+    // res.setHeader('Access-Control-Max-Age',86400);
+    res.send('allowed.');
+});
+
+app.use(express.static('public'));
+
 app.listen(3000)
